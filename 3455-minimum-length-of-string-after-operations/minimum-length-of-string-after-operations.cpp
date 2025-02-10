c79f76868103e58b6786fbb59bf5058ea2fbd58a
phuonghodacminh <phuonghodacminh@gmail.com>
2025-02-10 21:39:35 +0700

    Time: 17 ms (51.19%) | Memory: 31.9 MB (87.41%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        unordered_map<char, int> freq;
+        vector<int> freq(26, 0);
         int n = s.size();
         for(int i = 0; i < n; i++){
-            freq[s[i]]++;
+            freq[s[i] - 'a']++;
         }
         int min = 0;
-        for(auto it = freq.begin(); it != freq.end(); it++){
-            int c = it -> second;
-            if(c % 2) min++;
+        for(int i = 0; i < 26; i++){
+            if(freq[i] == 0);
+            else if(freq[i] % 2) min++;
             else min += 2;
         }
         return min;
