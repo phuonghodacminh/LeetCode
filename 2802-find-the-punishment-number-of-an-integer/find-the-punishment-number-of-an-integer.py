130b65891aa43e8bdabc86aa9c8941784afe7490
phuonghodacminh <phuonghodacminh@gmail.com>
2025-02-15 10:07:09 +0700

    Time: 2234 ms (25.00%) | Memory: 12.5 MB (0.00%) - LeetSync

---
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def punishmentNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = 0
+        for i in range(1, n + 1):
+            square = i * i
+            s = str(square)
+            if self.backtrack(s, 0, 0, i):
+                print(square)
+                count += square 
+                
+        return count
+    
+    def backtrack(self, s, idx, check, i):
+        if idx == len(s):
+            if check == i:
+                return True
+            else:
+                return False
+        
+        for l in range(1, len(s) - idx + 1):
+            t = s[idx: idx + l]
+            num = int(t)
+            if self.backtrack(s, idx + l, check + num, i):
+                return True
+
+        return False
+
+            
+            
+
+        
