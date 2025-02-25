c46f30aa74c381c289add28774f682e00832d7aa
phuonghodacminh <phuonghodacminh@gmail.com>
2025-02-25 10:16:06 +0700

    Time: 4 ms (46.48%) | Memory: 16.9 MB (62.78%) - LeetSync

--- 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.cpp
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     vector<int> intersection(vector<vector<int>>& nums) {
         unordered_map<int, int> check;
+        vector<int> result;
         int n = nums.size();
         for (int i = 0; i < n; i++){
             for (int j = 0; j < nums[i].size(); j++){
                 check[nums[i][j]]++;
+                if(check[nums[i][j]] == n) result.push_back(nums[i][j]);
             }
         }
-        vector<int> result;
-        for (int i = 1; i < 1001; i++){
-            if(check[i] == n) result.push_back(i);
-        }
+        sort(result.begin(), result.end());
         return result;
     }
 };
