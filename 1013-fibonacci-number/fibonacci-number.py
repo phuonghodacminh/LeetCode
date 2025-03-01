44d306867f01efca894eefee1dd3041d9f380afa
phuonghodacminh <phuonghodacminh@gmail.com>
2025-03-01 08:30:30 +0700

    Time: 19 ms (56.79%) | Memory: 12.4 MB (52.03%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -2,11 +2,8 @@ class Solution(object):
     def dp(self, n, memoization):
         if memoization[n] != -1:
             return memoization[n]
-        else:
-            memoization[n - 1] = self.dp(n - 1, memoization) 
-            memoization[n - 2] = self.dp(n - 2, memoization)
-            memoization[n] = memoization[n - 1] + memoization[n - 2]
-            return memoization[n]
+        memoization[n] = self.dp(n - 1, memoization) + self.dp(n - 2, memoization)
+        return memoization[n]
         
     def fib(self, n):
         """
@@ -15,10 +12,6 @@ class Solution(object):
         """
         if n == 0:
             return 0
-        memoization = [-1 for _ in range(n + 1)]
-        memoization[0] = 0
-        memoization[1] = 1
-        self.dp(n, memoization)
-        return memoization[n]
-
-        
+        memoization = [-1] * (n + 1)
+        memoization[0], memoization[1] = 0, 1
+        return self.dp(n, memoization)
